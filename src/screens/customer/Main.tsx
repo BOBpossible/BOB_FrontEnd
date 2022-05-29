c7ced97f91775739217f13edcac12786a006a852
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-29 21:55:53 +0900

    feat: add homeMission component

--- src/screens/customer/Main.tsx
+++ src/screens/customer/Main.tsx
@@ -1,18 +1,63 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
 import {Colors} from 'react-native-paper';
+import HomeMission from '../../components/HomeMission';
+
+const dummyMission = [
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+  },
+];
 
 const Main = () => {
-  const title = 'Main';
   return (
-    <View style={[styles.flex]}>
-      <Text>{title}</Text>
+    <View style={[styles.mainView]}>
+      <Text>애니메이션 헤더</Text>
+      <FlatList
+        data={dummyMission}
+        renderItem={({item}) => (
+          <HomeMission
+            name={item.name}
+            category={item.category}
+            day={item.day}
+            minCost={item.minCost}
+            point={item.point}
+          />
+        )}
+        ListHeaderComponent={<Text>My Mission</Text>}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  flex: {flex: 1},
+  mainView: {flex: 1, margin: 10},
 });
 
 export default Main;
