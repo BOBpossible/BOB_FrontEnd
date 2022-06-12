4f6ad65c62b1d061e3656193f5196d8d1d5618f4
yejinleee <yejin9487@gmail.com>
2022-06-12 23:42:03 +0900

    Feat: '진행완료'스위치 화면 MissionSuccessList

---
+++ src/components/MissionSuccessList.tsx
@@ -0,0 +1,132 @@
+import React, {useRef} from 'react';
+import {View, StyleSheet, Text, Animated, FlatList} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {MissionCard} from '../components';
+import {AnimatedHeader} from '../components';
+import {useRecoilState} from 'recoil';
+import {userToken} from '../state';
+const dummyMission = [
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+    completeMonth: 2,
+    completeDate: 20,
+    completeDay: '월',
+    completeStatus: '미션 성공',
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+    completeMonth: 2,
+    completeDate: 20,
+    completeDay: '월',
+    completeStatus: '미션 성공',
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+    completeMonth: 2,
+    completeDate: 20,
+    completeDay: '월',
+    completeStatus: '미션 성공',
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+    completeMonth: 2,
+    completeDate: 20,
+    completeDay: '월',
+    completeStatus: '미션 성공',
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+    completeMonth: 2,
+    completeDate: 20,
+    completeDay: '월',
+    completeStatus: '미션 성공',
+  },
+  {
+    name: '반이학생마라탕',
+    category: '중식당',
+    day: 7,
+    minCost: 10000,
+    point: 500,
+    completeMonth: 2,
+    completeDate: 20,
+    completeDay: '월',
+    completeStatus: '미션 성공',
+  },
+];
+///////////////////////////////////////////////////FlatList 스크롤안되요 ㅜㅜ ISSUE
+export const MissionSuccessList = () => {
+  const [token, setToken] = useRecoilState(userToken);
+  console.log(token);
+  const offset = useRef(new Animated.Value(0)).current;
+  const insets = useSafeAreaInsets();
+  console.log('inset?', insets);
+  function review() {
+    console.log('잘먹었습니다~');
+  }
+  const styles = StyleSheet.create({
+    missionDate: {
+      alignItems: 'center',
+      marginBottom: 2,
+    },
+    missionDateText: {
+      color: '#616161',
+      fontSize: 14,
+    },
+  });
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <FlatList
+        contentContainerStyle={{paddingBottom: 10}}
+        scrollEventThrottle={10}
+        data={dummyMission}
+        renderItem={({item}) => (
+          <>
+            <View style={[styles.missionDate]}>
+              <Text>
+                {item.completeMonth}/{item.completeDate}({item.completeDay}) • {item.completeStatus}
+              </Text>
+            </View>
+            <MissionCard
+              name={item.name}
+              category={item.category}
+              day={item.day}
+              minCost={item.minCost}
+              point={item.point}
+              status="review"
+              handleOnPress={review}
+            />
+          </>
+        )}
+        ListHeaderComponent={
+          <View style={{flexDirection:'row', marginLeft: 16, marginRight: 16, marginTop: 9, marginBottom: 12}}>
+            <Text style={{fontSize: 18, color: '#616161'}}>최근 일주일</Text>
+            <Text> v</Text>
+            {/* 이거 토글 ? ?  */}
+          </View>
+        }
+        ItemSeparatorComponent={() => <View style={{margin: 16}} />}
+      />
+    </SafeAreaView>
+  );
+};
