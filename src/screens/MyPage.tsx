45d8d039636b532d71886e3e53dd252477b686fc
yejinleee <yejin9487@gmail.com>
2022-06-21 01:55:41 +0900

    Feat: 마이페이지 수정 중

--- src/screens/MyPage.tsx
+++ src/screens/MyPage.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {Colors} from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {MyUser} from '../components/MyUser';
 
 const MyPage = () => {
+  const [status, setStatus] = useState<string>('request'); //버튼문구 //"start","request","onrequest","success", "review"//
+
   const title = 'MyPage';
   const storeData = async (value: string) => {
     try {
@@ -15,8 +18,18 @@ const MyPage = () => {
   const logout = () => {
     storeData('');
   };
+  console.log(AsyncStorage.getItem('userToken'));
   return (
     <View style={[styles.flex]}>
+      <View style={[styles.headerWrap]}>
+        <View style={[styles.header]}>
+          <Text style={[styles.headerText]}>마이페이지</Text>
+        </View>
+      </View>
+      <MyUser username={'춘식이'} userid={123} status={status} />
+      <View style={[styles.userWrap]}>
+        <Text>냐</Text>
+      </View>
       <Text>{title}</Text>
       <TouchableOpacity onPress={logout}>
         <View style={{height: 200, width: 200, borderWidth: 1}}>
@@ -29,6 +42,29 @@ const MyPage = () => {
 
 const styles = StyleSheet.create({
   flex: {flex: 1},
+  headerWrap: {
+    width: '100%',
+    borderBottomColor: '#DFDFDF',
+    borderBottomWidth: 1,
+    marginBottom: 8,
+  },
+  header: {
+    height: 40,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 17,
+    color: 'black',
+    marginLeft: 16,
+    marginRight: 16,
+    fontWeight: '600',
+  },
+  userWrap: {
+    width: '100%',
+    borderBottomColor: '#DFDFDF',
+    borderBottomWidth: 1,
+  },
 });
 
 export default MyPage;
