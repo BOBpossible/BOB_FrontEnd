619ecb093d1d886210a3c6516c938b2865f001a1
yejinleee <yejin9487@gmail.com>
2022-06-25 00:38:25 +0900

    Feat: Mypage MyUser Component

--- src/screens/MyPage.tsx
+++ src/screens/MyPage.tsx
@@ -5,9 +5,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MyUser} from '../components/MyUser';
 
 const MyPage = () => {
-  const [status, setStatus] = useState<string>('request'); //버튼문구 //"start","request","onrequest","success", "review"//
+  const [status, setStatus] = useState<string>('unidentified'); //인증된 - "identified", 미인증- "unidentified"
 
-  const title = 'MyPage';
   const storeData = async (value: string) => {
     try {
       await AsyncStorage.setItem('userToken', value);
@@ -26,11 +25,10 @@ const MyPage = () => {
           <Text style={[styles.headerText]}>마이페이지</Text>
         </View>
       </View>
-      <MyUser username={'춘식이'} userid={123} status={status} />
+      <MyUser username={'춘식이'} userEmail={'baegopa@bob.com'} point={2500} status={status} />
       <View style={[styles.userWrap]}>
         <Text>냐</Text>
       </View>
-      <Text>{title}</Text>
       <TouchableOpacity onPress={logout}>
         <View style={{height: 200, width: 200, borderWidth: 1}}>
           <Text>로그아웃</Text>
@@ -46,7 +44,6 @@ const styles = StyleSheet.create({
     width: '100%',
     borderBottomColor: '#DFDFDF',
     borderBottomWidth: 1,
-    marginBottom: 8,
   },
   header: {
     height: 40,
