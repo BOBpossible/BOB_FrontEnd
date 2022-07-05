95a43713315f30c17347209b3934645a058517d1
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-07-05 21:44:50 +0900

    Fix: apple for ios only

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -19,9 +19,11 @@ const Login = ({}) => {
 
   useEffect(() => {
     // onCredentialRevoked returns a function that will remove the event listener. useEffect will call this function when the component unmounts
-    return appleAuth.onCredentialRevoked(async () => {
-      console.warn('If this function executes, User Credentials have been Revoked');
-    });
+    if (Platform.OS === 'ios') {
+      return appleAuth.onCredentialRevoked(async () => {
+        console.warn('If this function executes, User Credentials have been Revoked');
+      });
+    }
   }, []); // passing in an empty array as the second argument ensures this is only ran once when component mounts initially.
 
   //실행시 구글 로그인 설정 + 로그인 확인 코드
