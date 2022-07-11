1a374408815c3c968b2b19bfd902c79d12959b39
yejinleee <yejin9487@gmail.com>
2022-07-11 17:25:42 +0900

    Feat: Mypage- 내포인트 useInfiniteQuery FlatList 연결성공

--- src/api/store.ts
+++ src/api/store.ts
@@ -5,3 +5,13 @@ export const getStores = async (storeId: number) => {
   const {data} = await customAxios().get(`/api/v1/stores/${storeId}`);
   return data.result;
 };
+//마이페이지 - 나의 포인트 내역 조회
+export const getPointsList = async ({pageParam = 0}) => {
+  const response = await customAxios().get('/api/v1/points/list/me', {
+    params: {
+      page: pageParam,
+      size: 5,
+    },
+  });
+  return response;
+};
