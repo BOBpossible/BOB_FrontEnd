ddbca0f3427ab208d2b561547b3aa1a59bbf8b11
yejinleee <yejin9487@gmail.com>
2022-07-13 14:35:38 +0900

    Style: 미션 성공 포인트 적립 애니메이션

--- src/modal/DoneModal.tsx
+++ src/modal/DoneModal.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import type {FC} from 'react';
-import {Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Animated, Image, Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {DesignSystem} from '../assets/DesignSystem';
 
@@ -12,11 +12,53 @@ type DoneModalProps = {
 };
 
 const DoneModal: FC<DoneModalProps> = ({visible, closeDoneModal, category, point}) => {
+  // const scaleAnim = useRef(new Animated.Value(1)).current;
+  const scaleAnim = new Animated.Value(1);
+  const animateTextSize = (n: number) => {
+    Animated.timing(scaleAnim, {
+      toValue: n,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+  animateTextSize(3);
   return (
     <Modal visible={visible} animationType="fade">
       <View style={[styles.flex]}>
         <View style={[styles.flex, DesignSystem.centerArrange]}>
-          <Icon name="check" size={71} color="#6C69FF" />
+          {category === '성공' ? (
+            <Animated.View style={{marginTop: 20, transform: [{scale: scaleAnim}]}}>
+              <Image
+                style={{
+                  width: 90,
+                  height: 69,
+                  position: 'relative',
+                  // left: -40,
+                  // top: -20,
+                }}
+                source={require('../assets/images/pollen.png')}
+              />
+              <Image
+                style={{
+                  width: 30,
+                  height: 30,
+                  position: 'relative',
+                  left: 30,
+                  top: -40,
+                }}
+                source={require('../assets/images/coin.png')}
+              />
+              <Text
+                style={{
+                  fontSize: 9,
+                  position: 'relative',
+                  left: 35,
+                  top: -65,
+                }}>{point}P</Text>
+            </Animated.View>
+          ) : (
+            <Icon name="check" size={71} color="#6C69FF" />
+          )}
           {category === '리뷰' ? (
             <Text style={[styles.purpleTitleText]}>리뷰 등록 완료!</Text>
           ) : category === '입금' ? (
@@ -26,18 +68,11 @@ const DoneModal: FC<DoneModalProps> = ({visible, closeDoneModal, category, point
                 입금은 심사 후 매주 수요일에 일괄 송금됩니다.
               </Text>
             </>
-          ) : category === '성공' ? (
-            <>
-              <Text style={[styles.blackTitleText]}>미션 성공!</Text>
-              <Text style={[DesignSystem.body2Lt, {color: '#616161', marginTop: 9}]}>
-                {point}P가 적립되었습니다!
-              </Text>
-            </>
           ) : category === '문의' ? (
             <>
-              <Text style={[styles.blackTitleText]}>문의 성공!</Text>
+              <Text style={[styles.blackTitleText]}>문의 접수 완료!</Text>
               <Text style={[DesignSystem.body2Lt, {color: '#616161', marginTop: 9}]}>
-                문의 접수 완
+                문의가 접수되었습니다.
               </Text>
             </>
           ) : (
@@ -88,82 +123,3 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
 });
-
-// import {NativeStackScreenProps} from '@react-navigation/native-stack';
-// import React, {FC, useEffect, useState} from 'react';
-// import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
-// import {SafeAreaView} from 'react-native-safe-area-context';
-// import {MyStackParamList} from '../nav/MyNavigator';
-// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-// import {DesignSystem} from '../assets/DesignSystem';
-
-// type DoneScreenProps = {
-//   goNext?: () => void;
-//   category?: string;
-//   point?: number;
-// };
-// type Props = NativeStackScreenProps<MyStackParamList, 'DoneScreen'>;
-
-// const DoneScreen = ({navigation, route}: Props) => {
-// //   const goNext = () => {
-// //     navigation.reset({routes: [{name: 'MainNavigator'}]});
-// //   };
-//   const goNext = useState(route.params.goNext);
-//   const category = useState<string>(route.params.category);
-//   const point = route.params.point;
-//   console.log('category: ', category);
-//   useEffect(() => {
-//     const id = setTimeout(() => {
-//       navigation.navigate('HowTo1');
-//     }, 1500);
-//     return () => {
-//       clearTimeout(id);
-//     };
-//   }, []);
-
-//   return (
-//     <SafeAreaView style={[styles.flex, {backgroundColor: 'white'}]}>
-//       <View style={[styles.flex, DesignSystem.centerArrange]}>
-//         <Icon name="check" size={71} color="#6C69FF" />
-//         {String(category) === '리뷰' ? (
-//           <Text style={[styles.blackTitleText]}>리뷰 등록이 완료되었습니다.</Text>
-//         ) : String(category) === '입금' ? (
-//           <>
-//             <Text style={[styles.blackTitleText]}>입금 신청이 완료되었습니다.</Text>
-//             <Text style={[DesignSystem.body2Long, {color: '#616161', marginTop: 6}]}>
-//               입금은 심사 후 매주 수요일에 일괄 송금됩니다.
-//             </Text>
-//           </>
-//         ) : String(category) === '성공' ? (
-//           <>
-//             <Text style={[styles.blackTitleText]}>미션 성공!</Text>
-//             <Text style={[DesignSystem.body2Lt, {color: '#616161', marginTop: 9}]}>
-//               {point}P가 적립되었습니다!
-//             </Text>
-//           </>
-//         ) : (
-//           <Text style={[styles.purpleTitleText]}>가입완료</Text>
-//         )}
-//       </View>
-//       {String(category) === '리뷰' || String(category) === '입금' ? (
-//         <TouchableOpacity onPress={goNext} style={[styles.buttonWrap]}>
-//           <View style={[styles.buttonStyle]}>
-//             <Text style={[styles.buttonText]}>홈으로 돌아가기</Text>
-//           </View>
-//         </TouchableOpacity>
-//       ) : (
-//         <TouchableOpacity onPress={goNext} style={[styles.buttonWrap]}>
-//           <View style={[styles.buttonStyle]}>
-//             <Text style={[styles.buttonText]}>확인</Text>
-//           </View>
-//         </TouchableOpacity>
-//       )}
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-
-// });
-
-// export default DoneScreen;
