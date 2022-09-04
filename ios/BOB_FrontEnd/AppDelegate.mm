ce1dcf8ca133293c8476fb1417050e6885393838
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-09-04 18:19:22 +0800

    init: ios codepush setting

--- ios/BOB_FrontEnd/AppDelegate.mm
+++ ios/BOB_FrontEnd/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import <Firebase.h>
 #import "AppDelegate.h"
+#import <CodePush/CodePush.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -64,7 +65,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
