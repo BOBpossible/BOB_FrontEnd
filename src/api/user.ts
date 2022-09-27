08372cd98839cc97f77a09a20f36be454eb8ce68
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-09-27 12:46:16 +0800

    Feat: add edit page category list

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,3 +4,13 @@ export const getUserInfo = async () => {
   const {data} = await customAxios().get('/api/v1/users/me');
   return data.result;
 };
+
+export const getCategoryList = async () => {
+  const {data} = await customAxios().get('/api/v1/categories');
+  return data.result;
+};
+
+export const getUserCategory = async () => {
+  const {data} = await customAxios().get('/api/v1/member-categories/me');
+  return data.result;
+};
--- src/screens/my/MyEditUserInfo.tsx
+++ src/screens/my/MyEditUserInfo.tsx
@@ -15,13 +15,15 @@ import {DesignSystem} from '../../assets/DesignSystem';
 import {IgetUsersMe} from '../../data';
 import {useQuery} from 'react-query';
 import {queryKey} from '../../api/queryKey';
-import {getUserInfo} from '../../api';
+import {getUserCategory, getUserInfo} from '../../api';
+import {getCategoryList} from '../../api';
 
 type Props = NativeStackScreenProps<MyStackParamList, 'MyEditUserInfo'>;
 
 export const MyEditUserInfo = ({navigation}: Props) => {
-  const {data, isError, refetch, isLoading} = useQuery<IgetUsersMe>(queryKey.USERINFO, getUserInfo);
-
+  const userInfo = useQuery<IgetUsersMe>(queryKey.USERINFO, getUserInfo);
+  const categoryList = useQuery('categoryList', getCategoryList);
+  const userCateogyList = useQuery('userCategoryList', getUserCategory);
   const goBack = () => {
     navigation.goBack();
   };
@@ -29,6 +31,7 @@ export const MyEditUserInfo = ({navigation}: Props) => {
     console.log('수정');
   }
 
+  console.log(userCateogyList.data);
   return (
     <>
       <SafeAreaView style={{flex: 0, backgroundColor: '#FFFFFF'}} />
@@ -41,21 +44,55 @@ export const MyEditUserInfo = ({navigation}: Props) => {
               source={require('../../assets/images/bobProfile.png')} //
             />
           </TouchableOpacity>
-          <Text style={[styles.usernameText]}>{data?.name}님</Text>
+          <View style={[{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}]}>
+            <Text style={[DesignSystem.body1Lt, DesignSystem.grey17]}>{userInfo.data?.name}</Text>
+            <TouchableOpacity style={{}}>
+              <View style={[styles.editButton]}>
+                <Text style={[DesignSystem.caption1Lt, DesignSystem.grey10]}>수정</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
         <View style={[styles.userInfoEdit]}>
           <View style={[styles.userInfoEditContent]}>
             <View style={[styles.emailNinput]}>
               <Text style={[DesignSystem.title4Md, {color: 'black', marginBottom: 8}]}>이메일</Text>
-              <Text style={[DesignSystem.body1Lt, DesignSystem.grey8]}>{data?.email}</Text>
+              <Text style={[DesignSystem.body1Lt, DesignSystem.grey8]}>{userInfo.data?.email}</Text>
             </View>
             <View>
               <Text style={[DesignSystem.title4Md, {color: 'black', marginBottom: 8}]}>
                 전화번호
               </Text>
               <Text style={[DesignSystem.body1Lt, DesignSystem.grey8]}>
-                010 - **** - {data?.phone}
+                010 - **** - {userInfo.data?.phone}
               </Text>
+            </View>
+          </View>
+        </View>
+        <View style={[styles.userInfoEdit, {marginTop: 8}]}>
+          <View style={[styles.userInfoEditContent]}>
+            <Text style={[DesignSystem.title4Md, {color: 'black', marginBottom: 8}]}>
+              선호하는 음식
+            </Text>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}
+            >
+              <View style={{flexDirection: 'row'}}>
+                {userCateogyList.data &&
+                  userCateogyList.data?.map((item: any, i: number) => {
+                    return (
+                      <Text style={[DesignSystem.body1Lt, DesignSystem.grey8]}>
+                        {item.name}
+                        {userCateogyList.data.length - 1 !== i ? ', ' : ''}
+                      </Text>
+                    );
+                  })}
+              </View>
+              <TouchableOpacity style={{}}>
+                <View style={[styles.editButton]}>
+                  <Text style={[DesignSystem.caption1Lt, DesignSystem.grey10]}>수정</Text>
+                </View>
+              </TouchableOpacity>
             </View>
           </View>
         </View>
@@ -77,7 +114,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'column',
   },
-  profileWrap: {},
+  profileWrap: {marginBottom: 8},
   profileImg: {
     width: 70,
     height: 70,
@@ -93,11 +130,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 53,
     top: 45,
-  },
-  usernameText: {
-    color: '#000000',
-    fontSize: 16,
-    // fontWeight: 'bold',
   },
   userInfoEdit: {
     backgroundColor: '#FFFFFF',
@@ -128,5 +160,14 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginRight: 16,
     marginTop: 8,
+  },
+  editButton: {
+    height: 20,
+    width: 37,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+    backgroundColor: '#EFEFEF',
+    marginLeft: 8,
   },
 });
